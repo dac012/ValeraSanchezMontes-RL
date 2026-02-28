76b1056b19f4b83d9dabb361d701854ba1762e27
David <davidvaleralopez012@gmail.com>
2026-02-28 11:02:39 +0100

    estructura agents creada

---
+++ parte2/agents/AgentMonteCarloOnPolicy.py
@@ -0,0 +1,8 @@
+import gymnasium as gym
+
+class AgentMonteCarloOnPolicy:
+    def __init__(self, env:gym.Env, hiperparametros):
+
+    def get_action(self, state):
+    
+    def update(self, state, action, next_state, reward, terminated, truncated, info):
